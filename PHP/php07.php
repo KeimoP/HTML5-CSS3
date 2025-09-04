f70f4983df29bdb4b6a2b258d044c38b88e49dbb
KeimoP <keimo@hkhk.edu.ee>
2025-09-04 10:52:35 +0300

    Update php07.php

--- PHP/php07.php
+++ PHP/php07.php
@@ -8,6 +8,54 @@
   </head>
   <body>
     <div class="container">
+      <?php
+        echo "<br>"; 
+        function tere() {
+          return "Tere päiksekesekene!";
+        }
+        echo tere();
+        echo "<br>";
+        echo "<br>";
+        function uudised() {
+          echo '<form>';
+          echo '<div class="form-group">';
+          echo '<label for="exampleInputEmail1">Email address</label>';
+          echo '<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">';
+          echo '<small id="emailHelp" class="form-text text-muted">Liitu meie uudiskirjaga.</small>
+          </div>';
+        }
+        
+        echo uudised();
+      ?>
+        <button onclick="<?php echo "Oled liitunud meie uudiskirjaga";?>" type="submit" class="btn btn-primary">Liitu</button>
+      </form>
+      <br>
+      <br>
+      <br>
+      <?php
+            function kasutajanimi() {
+              if (isset($_GET['name1'])) {
+                $nimi = strtolower($_GET['name1']);
+                $nimituhikuta = str_replace(' ','',$nimi);
+                  echo $nimituhikuta."@hkhk.edu.ee";
+                  $tahed = str_shuffle($nimituhikuta);
+                  $numbird = [1,2,3,4,5,6,7,8,9];
+                  $numbridshuffled = shuffle($numbird);
+                  echo $nimituhikuta;
+                }
+            }
+      ?>
+      <form method="get">
+        <div class="form-group">
+          <label>Nimi</label>
+          <input type="text" name="name1" class="form-control" placeholder="Sisesta nimi">
+          <small class="form-text text-muted">Loome sulle @hkhk.edu.ee</small>
+        </div>
+      </form>
+      <?php 
+        kasutajanimi();
+      ?>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
